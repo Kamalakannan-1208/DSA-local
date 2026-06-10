d67f1fc8b7671e505f54eefdde04645c8266f090
Kamalakannan-1208 <kamalakannan.m1208@gmail.com>
2026-06-10 08:11:51 +0530

    updated next permutation

--- Array/next_permutation.py
+++ Array/next_permutation.py
@@ -1,3 +1,20 @@
+#brute force
+from itertools import permutations
+arr=[2,1,5,4,3,0,0]
+def next_permutation_brute_force(arr):
+    perms =sorted(set(permutations(arr)))
+    current = tuple(arr)
+    for i in range(len(perms)):
+        if perms[i] == current:
+            if i == len(perms) - 1:
+                return list(perms[0])
+            else:
+                return list(perms[i+1])
+    return arr
+
+print(next_permutation_brute_force(arr))
+#TC: O(n*n!) and SC: O(n*n!)
+#optimal
 arr=[2,1,5,4,3,0,0]
 index=-1
 def next_permutation(arr):
@@ -9,13 +26,13 @@ def next_permutation(arr):
         arr.reverse()
         return arr
     for j in range(len(arr)-1, index, -1):
-        print(arr[j],arr[index])
-        print(arr)
         if arr[j] > arr[index]:
             arr[index], arr[j] = arr[j], arr[index]
             break
     arr[index+1:] = reversed(arr[index+1:])
     return arr
+
+#TC: O(n) and SC: O(1)
     
 
 
